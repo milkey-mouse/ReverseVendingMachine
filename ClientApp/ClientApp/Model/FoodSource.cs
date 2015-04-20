1de84c64ecb6766796febca65ec1484f3eb9f515
Milkey Mouse <sillystixco@hotmail.com>
2015-04-19 21:06:21 -0700

    Finished data bind stuff (mostly)

---
+++ ClientApp/ClientApp/Model/FoodSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace ClientApp.Model
+{
+    class FoodSource
+    {
+        public string Title { get; set; }
+        public ObservableCollection<Food> Foods { get; set; }
+    }
+}
--- ClientApp/ClientApp/WindowBorderLess.xaml.cs
+++ ClientApp/ClientApp/WindowBorderLess.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using ClientApp;
+using ClientApp.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
+
 namespace BorderLess
 {
     /// <summary>
@@ -7,9 +13,36 @@ namespace BorderLess
     /// </summary>
     public partial class WindowBorderLess
     {
+        private ClientApp.Model.FoodSource _foodSource;
+
         public WindowBorderLess()
         {
             InitializeComponent();
+
+            _foodSource = new ClientApp.Model.FoodSource();
+            _foodSource.Title = "Runtime Title";
+
+            _foodSource.Foods = new ObservableCollection<ClientApp.Model.Food>()
+                {
+                };
+
+            for (int i = 1; i <= 20; i++)
+            {
+                _foodSource.Foods.Add(new ClientApp.Model.Food()
+                {
+                    Category = "Category " + i.ToString() + "/",
+                    Title = "Food " + i.ToString(),
+                });
+            }
+
+            _foodSource.Foods.Add(new ClientApp.Model.Food()
+            {
+                Category = "First category/",
+                Title = "First food",
+            });
+            
+
+            DataContext = _foodSource;
         }
 
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
