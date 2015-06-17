87efab5ec1280a8a6c4ca055a2c86df75e4d15a3
Milkey Mouse <sillystixco@hotmail.com>
2015-06-16 18:50:53 -0700

    Windows 10 universal app start

---
+++ PiClient/piconn.ps1
@@ -0,0 +1,3 @@
+﻿net start WinRM
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value rvm -force
+Enter-PsSession -ComputerName rvm -Credential rvm\Administrator #pass is "CorrectHorseBatteryStaple"
